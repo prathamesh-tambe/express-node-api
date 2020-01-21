e07cc7e2f2f770e7db5342bb561d440643f7c594
Prathamesh Tambe <tambe.p@husky.neu.edu>
2020-01-21 12:26:37 -0500

    update contact information added

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -22,3 +22,21 @@ export const getContacts = (req,res) => {
         res.json(contact);
     })
 }
+
+export const getContactsWithId = (req,res) => {
+    Contact.findById(req.params.contactId, (err, contact) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(contact);
+    })
+}
+
+export const updateContact = (req,res) => {
+    Contact.findOneAndUpdate({_id : req.params.contactId},req.body,{ new:true,useFindAndModify:false }, (err, contact) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(contact);
+    })
+}
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,4 +1,4 @@
-import {addNewContact,getContacts} from '../controllers/crmController';
+import {addNewContact,getContacts,getContactsWithId,updateContact} from '../controllers/crmController';
 
 const routes = (app) =>{
     app.route('/contact')
@@ -6,14 +6,13 @@ const routes = (app) =>{
         .post(addNewContact);        
     
     app.route('/contact/:contactId')
+        .get(getContactsWithId)
         .put((req,res,next) => {
             console.log("This is middleware request");
             console.log(`Request from : ${req.originalUrl}`);
             console.log(`Request from : ${req.method}`);
             next();
-        },(req,res,next) => {
-            res.send(`contact put api working ${req.params.contactId}`);
-        })
+        },updateContact)
 
         .delete((req,res) => res.send(`contact delete api working ${req.params.contactId}`));        
 
